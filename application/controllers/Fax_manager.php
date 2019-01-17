826017b6354e490295ad54e1f45ec084c2ec08b7
Adi <Adi@Adi-PC>
2019-01-18 00:48:23 +0530

    fax manager - time try 1

--- application/controllers/Fax_manager.php
+++ application/controllers/Fax_manager.php
@@ -1,6 +1,9 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Fax_manager extends CI_Controller {
+
     public function index() {
         if (isset($argv)) {
             if (!empty($argv[1])) {
@@ -13,12 +16,14 @@ class Fax_manager extends CI_Controller {
             }
         }
     }
-    public function hwaBoWSDmTNblPFakqzEhzASerOeKGAc() {
 
-        $cur_time = DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s', strtotime("-5 hour")));
+    public function hwaBoWSDmTNblPFakqzEhzASerOeKGAc() {
+        log_message("error", "===========================> version => 1.0");
+        $cur_time = DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s'));
         // echo "system time = " . json_encode(date('Y-m-d H:i:s')) . "<br/>";
         // echo "-6 hours = " . json_encode($cur_time) . "<br/>";
-        $sStartDate = "20180606"; //$cur_time->sub(new DateInterval('PT5M'))->format("Ymd");
+//        $sStartDate = "20180606"; //$cur_time->sub(new DateInterval('PT5M'))->format("Ymd");
+        $sStartDate = $cur_time->sub(new DateInterval('PT5M'))->format("Ymd");
         $sEndDate = $cur_time->format('Ymd');
         log_message("error", "start = " . $sStartDate . ", and end = " . $sEndDate);
         // echo "start = " . $sStartDate . ", and end = " . $sEndDate . "<br/>";
@@ -35,7 +40,7 @@ class Fax_manager extends CI_Controller {
             $clinic_id = $clinic->id;
             $access_id = $clinic->srfax_account_num;
             $access_pwd = $clinic->srfax_pass;
-            $caller_id =$clinic->srfax_number;
+            $caller_id = $clinic->srfax_number;
             $sender_mail = $clinic->srfax_email;
 
             $postVariables = array(
@@ -48,7 +53,7 @@ class Fax_manager extends CI_Controller {
                 // "sStartDate" => "20180201",
                 // "sEndDate" => "20180331",
                 "sIncludeSubUsers" => "Y",
-                "sViewedStatus" => "UNREAD"
+//                "sViewedStatus" => "UNREAD"
             );
             $curlDefaults = array(
                 CURLOPT_POST => 1,
@@ -66,39 +71,48 @@ class Fax_manager extends CI_Controller {
             curl_setopt_array($ch, $curlDefaults);
             $result = json_decode(curl_exec($ch));
 
-            $start_datetime = $cur_time->sub(new DateInterval('PT0M'));
-    //        $end_datetime = date('Y-m-d H:i:s');
-            log_message("error", "Start DT = " . json_encode($start_datetime)); // . " , and end DT = " . json_encode($end_datetime);
-            echo "Start DT = " . json_encode($start_datetime) . "<br/>";
+            $before_5_mins = $cur_time->sub(new DateInterval('PT0M'));
+            //        $end_datetime = date('Y-m-d H:i:s');
+            log_message("error", "Before 5 min time = " . json_encode($before_5_mins)); // . " , and end DT = " . json_encode($end_datetime);
+//            echo "Start DT = " . json_encode($start_datetime) . "<br/>";
             $faxes = $result->Result;
             log_message("error", "faxes = " . json_encode($faxes));
             foreach ($faxes as $fax) {
                 log_message("error", "fax = " . json_encode($fax));
-                $fax_details_id = substr($fax->FileName, strpos($fax->FileName, "|") + 1);
-                $this->db->select("id as family_physician_id");
-                $this->db->from("physician_info");
-                $this->db->where(array(
-                    "fax_number" => $fax->CallerID
-                ));
-                $result = $this->db->get()->result();
-                $from = ($result) ? $result[0]->family_physician_id : "";
-                $to = $clinic_id; // clinic id
-                $pages = $fax->Pages;
-                $sender_fax = $fax->CallerID;
-                if(strlen($sender_fax) == 10) {
-                    $sender_fax = "1$sender_fax";
+                $fax_date = DateTime::createFromFormat('M d/y h:i a', $fax->Date);
+                log_message("error", "check fax timing fax (".json_encode($before_5_mins) . ") and (".json_encode($fax_date) . ")");
+                
+                if ($fax_date > $before_5_mins) {
+                    log_message("error", "The fax was in time");
+                    $fax_details_id = substr($fax->FileName, strpos($fax->FileName, "|") + 1);
+                    $this->db->select("id as family_physician_id");
+                    $this->db->from("physician_info");
+                    $this->db->where(array(
+                        "fax_number" => $fax->CallerID
+                    ));
+                    $result = $this->db->get()->result();
+                    $from = ($result) ? $result[0]->family_physician_id : "";
+                    $to = $clinic_id; // clinic id
+                    $pages = $fax->Pages;
+                    $sender_fax = $fax->CallerID;
+                    if (strlen($sender_fax) == 10) {
+                        $sender_fax = "1$sender_fax";
+                    }
+                    log_message("error", "fax received. detail id = " . $fax_details_id . "<br/>");
+                    echo "fax details = $fax_details_id, $from, $to, $pages, $sender_fax" . "<br/>";
+                    $this->retrieve_fax($fax_details_id, $from, $to, $pages, $sender_fax, $access_id, $access_pwd);
                 }
-                log_message("error", "fax received. detail id = " . $fax_details_id . "<br/>");
-                echo "fax details = $fax_details_id, $from, $to, $pages, $sender_fax" . "<br/>";
-                $this->retrieve_fax($fax_details_id, $from, $to, $pages, $sender_fax, $access_id, $access_pwd);
+                else {
+                    log_message("error", "The fax was out of time");
+                }
             }
-
         }
-        
+
         log_message("error", "=======================================================================");
         log_message("error", "=======================================================================");
         log_message("error", "=======================================================================");
     }
+
     private function retrieve_fax($fax_details_id, $from, $to, $pages, $sender_fax, $access_id, $access_pwd) {
         $this->db->trans_start();
         $pdf_fax_file_name = "";
@@ -189,37 +203,38 @@ class Fax_manager extends CI_Controller {
         ));
         log_message("error", " ==================================== > saved fax files => $pdf_fax_file_name $tiff_fax_file_name");
         //make viewed status read
-        $postVariables = array(
-            "action" => "Update_Viewed_Status",
-            "access_id" => $access_id,
-            "access_pwd" => $access_pwd,
-            "sFaxDetailsID" => $fax_details_id,
-            "sDirection" => "IN",
-            "sMarkasViewed" => "Y"
-        );
-        $curlDefaults = array(
-            CURLOPT_POST => 1,
-            CURLOPT_HEADER => 0,
-            CURLOPT_URL => "https://www.srfax.com/SRF_SecWebSvc.php",
-            CURLOPT_FRESH_CONNECT => 1,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_FORBID_REUSE => 1,
-            CURLOPT_TIMEOUT => 60,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_SSL_VERIFYHOST => 2,
-            CURLOPT_POSTFIELDS => http_build_query($postVariables),
-        );
-        $ch = curl_init();
-        curl_setopt_array($ch, $curlDefaults);
-        $result = curl_exec($ch);
-        if (curl_errno($ch)) {
-            log_message("error", "Error – " . json_encode(curl_error($ch)));
-            return;
-        }
+//        $postVariables = array(
+//            "action" => "Update_Viewed_Status",
+//            "access_id" => $access_id,
+//            "access_pwd" => $access_pwd,
+//            "sFaxDetailsID" => $fax_details_id,
+//            "sDirection" => "IN",
+//            "sMarkasViewed" => "Y"
+//        );
+//        $curlDefaults = array(
+//            CURLOPT_POST => 1,
+//            CURLOPT_HEADER => 0,
+//            CURLOPT_URL => "https://www.srfax.com/SRF_SecWebSvc.php",
+//            CURLOPT_FRESH_CONNECT => 1,
+//            CURLOPT_RETURNTRANSFER => 1,
+//            CURLOPT_FORBID_REUSE => 1,
+//            CURLOPT_TIMEOUT => 60,
+//            CURLOPT_SSL_VERIFYPEER => false,
+//            CURLOPT_SSL_VERIFYHOST => 2,
+//            CURLOPT_POSTFIELDS => http_build_query($postVariables),
+//        );
+//        $ch = curl_init();
+//        curl_setopt_array($ch, $curlDefaults);
+//        $result = curl_exec($ch);
+//        if (curl_errno($ch)) {
+//            log_message("error", "Error – " . json_encode(curl_error($ch)));
+//            return;
+//        }
         log_message("error", "view status changed");
         echo "View status changed " . "<br/>";
         $this->db->trans_complete();
     }
+
     private function generate_random_string($length = 32) {
         $timestamp = time();
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
@@ -230,6 +245,5 @@ class Fax_manager extends CI_Controller {
         }
         return $timestamp . "_" . $randomString;
     }
+
 }
-
-
