308b126e1c512cf36aaf0b5feb41d24fa2f0faf6
Arun <arunk4868@gmail.com>
2019-02-01 12:12:41 +0530

    IVR dates added

--- application/controllers/Call_view.php
+++ application/controllers/Call_view.php
@@ -194,9 +194,12 @@ class Call_view extends CI_Controller {
 			echo "<Response>";
 			echo "<Gather  timeout='3' numDigits='1' action='$base_url/call_view/step_three?pname=" . urlencode($_GET['pname']) . "&amp;patient_lname=" . urlencode($_GET['patient_lname']) . "&amp;pvname=" . urlencode($_GET['pvname']) . "&amp;cname=" . urlencode($_GET['cname']) . "&amp;aDate=" . urlencode($_GET['aDate']) . "&amp;aTime=" . urlencode($_GET['aTime']) . "&amp;address=" . urlencode($_GET['address']) . "' method='GET'>";
 			echo "<Say  voice='Polly.Joanna'>Hi  " . $_GET['pname'] . ",  Please select one of the following dates and times for your appointment with " . $_GET['cname']."</Say>";
+			echo "<Pause length='1'/>";
 		    echo "<Say  voice='Polly.Joanna'>For  <say-as interpret-as='date' format='yyyymmdd'  detail='1'>".$data[0]['start_time']."</say-as> - please enter 1</Say>";
 		    echo "<Say  voice='Polly.Joanna'>For <say-as interpret-as='date' format='yyyymmdd'  detail='1'>".$data[1]['start_time']." </say-as>- please enter 2</Say>";
+			echo "<Pause length='1'/>";
 		    echo "<Say  voice='Polly.Joanna'>For <say-as interpret-as='date' format='yyyymmdd'  detail='1'>".$data[2]['start_time']."</say-as> - please enter 3</Say>";
+			echo "<Pause length='1'/>";
 		    echo "<Say  voice='Polly.Joanna'>If you would like the clinic to contact you directly - please enter 4</Say>";
 			echo "</Gather>";
 			echo "<Pause length='10'/>";
@@ -304,12 +307,13 @@ class Call_view extends CI_Controller {
             echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 			echo "<Response>";
 				echo "<Gather  timeout='3' numDigits='1' action='$base_url/call_view/step_three?pname=" . urlencode($_GET['pname']) . "&amp;patient_lname=" . urlencode($_GET['patient_lname']) . "&amp;pvname=" . urlencode($_GET['pvname']) . "&amp;cname=" . urlencode($_GET['cname']) . "&amp;aDate=" . urlencode($_GET['aDate']) . "&amp;aTime=" . urlencode($_GET['aTime']) . "&amp;address=" . urlencode($_GET['address']) . "' method='GET'>";
-				echo "<Say  voice='Polly.Joanna'>Hello " . $_GET['pname'] . " " . $_GET['patient_lname'] . "</Say>";
-				echo "<Say  voice='Polly.Joanna'>Hi  " . $_GET['pname'] . ",  Please select one of the following dates and times for your appointment with " . $_GET['cname']."</Say>";
 				echo "<Say  voice='Polly.Joanna'>Hi  " . $_GET['pname'] . ",  Please select one of the following dates and times for your appointment with " . $_GET['cname']."</Say>";
 				echo "<Say  voice='Polly.Joanna'>For  <say-as interpret-as='date' format='yyyymmdd'  detail='1'>".$data[0]['start_time']."</say-as> - please enter 1</Say>";
+				echo "<Pause length='1'/>";
 				echo "<Say  voice='Polly.Joanna'>For <say-as interpret-as='date' format='yyyymmdd'  detail='1'>".$data[1]['start_time']." </say-as>- please enter 2</Say>";
+				echo "<Pause length='1'/>";
 				echo "<Say  voice='Polly.Joanna'>For <say-as interpret-as='date' format='yyyymmdd'  detail='1'>".$data[2]['start_time']."</say-as> - please enter 3</Say>";
+				echo "<Pause length='1'/>";
 				echo "<Say  voice='Polly.Joanna'>If you would like the clinic to contact you directly - please enter 4</Say>";
 				echo "</Gather>";
 				echo "<Pause length='10'/>";
