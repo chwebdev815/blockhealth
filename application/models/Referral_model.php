e13196c757ee9f1e8e576fe1f8ce4224c00c4c61
Arun <arunk4868@gmail.com>
2019-02-01 11:13:55 +0530

    IVR dates added

--- application/models/Referral_model.php
+++ application/models/Referral_model.php
@@ -1609,7 +1609,7 @@ class Referral_model extends CI_Model {
         return $filtered;
     }
 
-    private function assign_slots($new_visit_duration) {
+    public function assign_slots($new_visit_duration) {
         //$clinic_id = $this->session->userdata("user_id");
         $clinic_id = 1;
         $next_day = DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s', strtotime("+1 day")));
