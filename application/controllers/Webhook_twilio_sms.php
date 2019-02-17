37c93c296aa31ab0cead2c893987068e96013ddb
Adi <Adi@Adi-PC>
2019-02-18 01:26:06 +0530

    call logs 1.4

--- application/controllers/Webhook_twilio_sms.php
+++ application/controllers/Webhook_twilio_sms.php
@@ -143,6 +143,7 @@ class Webhook_twilio_sms extends CI_Controller {
                         $referral_id = $this->db->select("c_ref.id")->from("clinic_referrals c_ref, referral_patient_info pat")->where(array(
                                     "pat.id" => $reserved->patient_id
                                 ))->get()->result()[0]->id;
+                        log_message("error", "referral id  = " . $referral_id . " , ". $this->db->last_query());
 
                         $this->db->where(array(
                             "id" => $referral_id
@@ -150,6 +151,8 @@ class Webhook_twilio_sms extends CI_Controller {
                             "accepted_status" => "Confirmed",
                             "accepted_status_icon" => "green"
                         ));
+                        
+                        log_message("error", "referral id  = " . $referral_id . " , ". $this->db->last_query());
 
 
                         $this->load->model("referral_model");
