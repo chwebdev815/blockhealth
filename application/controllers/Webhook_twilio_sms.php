70f5f5c82f2588f9ffbe19c8146c9a0d66d6cce1
Adi <Adi@Adi-PC>
2019-02-21 22:28:23 +0530

    new dates on expired visit book time on sms reply - 3

--- application/controllers/Webhook_twilio_sms.php
+++ application/controllers/Webhook_twilio_sms.php
@@ -188,6 +188,7 @@ class Webhook_twilio_sms extends CI_Controller {
                         $patient_data = $patient_data[0];
 
                         //find asignable slots
+                        $new_visit_duration = 30;
                         $this->load->model("referral_model");
                         $response = $this->referral_model->assign_slots($new_visit_duration, $visit->patient_id);
                         if ($response["result"] === "error") {
