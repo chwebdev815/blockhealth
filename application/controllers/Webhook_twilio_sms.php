16f63d8b5bdfd5c79b15337197486739acf4a77a
Arun <arunk4868@gmail.com>
2019-03-14 02:40:56 +0530

    SMS issue

--- application/controllers/Webhook_twilio_sms.php
+++ application/controllers/Webhook_twilio_sms.php
@@ -12,6 +12,7 @@ class Webhook_twilio_sms extends CI_Controller {
 //        $data = $this->input->get();
         log_message("error", "webhook incoming sms = " . json_encode($data));
       file_put_contents('/var/www/html/inSMS4.txt',json_encode($data));
+      file_put_contents('/var/www/html/inSMS5.txt','hello');
         $Body = strtoupper(trim($data["Body"]));
 //
         $From = $data["From"];
