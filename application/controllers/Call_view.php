a8d1fa1bd31524f0d784f0c1e4da1f466f978670
Adi <Adi@Adi-PC>
2019-03-16 01:23:04 +0530

    dynamic reminder for clinic wise - arun push

--- application/controllers/Call_view.php
+++ application/controllers/Call_view.php
@@ -143,8 +143,8 @@ class Call_view extends CI_Controller {
         . "reserved_id=" . urlencode($_GET["reserved_id"]) . "&amp;"
         . "notify_voice=" . urlencode($_GET["notify_voice"]) . "&amp;"
         . "notify_sms=" . urlencode($_GET["notify_sms"]) . "&amp;"
-        . "notify_email=" . urlencode($_GET["notify_email"]) . "&amp;
-                    Digits=timeout</Redirect>
+        . "notify_email=" . urlencode($_GET["notify_email"]) . 
+                "</Redirect>
 		</Response>";
     }
 
