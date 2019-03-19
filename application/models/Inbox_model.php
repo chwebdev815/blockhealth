5282fcad9632959087a9dadc5a1863b31edfdda9
Adi <Adi@Adi-PC>
2019-03-19 22:05:36 +0530

    whenever the SAVE or NEW REFERRAL row is added, separately, send API request, and default value for source parameter is 'remote'

--- application/models/Inbox_model.php
+++ application/models/Inbox_model.php
@@ -167,7 +167,7 @@ class Inbox_model extends CI_Model {
                 "username" => "hahmed",
                 "password" => "Blockhealth19",
                 "ClinicName" => "TCN",
-                "source" => "Remote",
+                "source" => "remote",
                 "PDFName" => "$new_file_name.pdf",
                 "PDFRemote" => base_url() . "uploads/physician_tasks/pdf/" . $new_file_name . ".pdf"
             ));
@@ -852,7 +852,7 @@ class Inbox_model extends CI_Model {
                         "username" => "hahmed",
                         "password" => "Blockhealth19",
                         "ClinicName" => "TCN",
-                        "source" => "Remote",
+                        "source" => "remote",
                         "PDFName" => "$file_new_name.pdf",
                         "PDFRemote" => base_url() . "uploads/health_records/" . $file_new_name . ".pdf"
                     ));
