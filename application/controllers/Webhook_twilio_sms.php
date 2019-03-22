fa3fd2c01ea1489042f9b45fcab533aec9ee9727
Adi <Adi@Adi-PC>
2019-03-23 03:10:37 +0530

    SMS Booking - send new dates for responses after 60 min limit (currently 10 min for testing)

--- application/controllers/Webhook_twilio_sms.php
+++ application/controllers/Webhook_twilio_sms.php
@@ -186,7 +186,7 @@ class Webhook_twilio_sms extends CI_Controller {
                                                     ->where("c_ref.id", "pat.referral_id", false)
                                                     ->get()->result()[0]->id;
 
-                                    $this->db->set("accepted_status_date", $reserved->create_datetime, false);
+                                    $this->db->set("accepted_status_date", $reserved->create_datetime);
                                     $this->db->where(array(
                                         "id" => $referral_id
                                     ))->update("clinic_referrals", array(
@@ -319,7 +319,7 @@ class Webhook_twilio_sms extends CI_Controller {
                                                         ->get()->result()[0]->id;
                                         
                                         log_message("error", "reserved is set to = " . json_encode($reserved));
-                                        $this->db->set("accepted_status_date", $reserved->create_datetime, false);
+                                        $this->db->set("accepted_status_date", $reserved->create_datetime);
                                         $this->db->where(array(
                                             "id" => $referral_id
                                         ))->update("clinic_referrals", array(
