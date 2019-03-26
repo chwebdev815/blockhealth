f28a5c1ab848c9773dcf082ff266250866e4de76
Adi <Adi@Adi-PC>
2019-03-27 00:54:07 +0530

    sms responses

--- application/controllers/Webhook_twilio_sms.php
+++ application/controllers/Webhook_twilio_sms.php
@@ -12,8 +12,8 @@ class Webhook_twilio_sms extends CI_Controller {
             $data = $_REQUEST;
 //        $data = $this->input->get();
             log_message("error", "webhook incoming sms = " . json_encode($data));
-            file_put_contents('/var/www/html/inSMS4.txt', json_encode($data));
-            file_put_contents('/var/www/html/inSMS5.txt', 'demo');
+//            file_put_contents('/var/www/html/inSMS4.txt', json_encode($data));
+//            file_put_contents('/var/www/html/inSMS5.txt', 'demo');
             //$Body = strtoupper(trim($data["Body"]));
 //
 
