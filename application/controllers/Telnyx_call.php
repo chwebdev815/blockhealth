93444c57470fb87eda43dc258a25cb26489f896b
Adi <Adi@Adi-PC>
2019-10-03 22:38:51 +0530

    $recording_saved

--- application/controllers/Telnyx_call.php
+++ application/controllers/Telnyx_call.php
@@ -200,7 +200,8 @@ class Telnyx_call extends CI_Controller {
         } elseif ($event_type == 'call.recording.saved' && base64_decode($payload['client_state']) == "name_recording_stop") {
 //            require 'functions.php';
             log_message("error", "inside call saved - " . json_encode($recording_saved));
-            if ($recording_saved->recording_saved == '1') {
+            if ($recording_saved->recording_saved == '0') {
+                log_message("error", "correct place");
                 log_message("error", "start - call.recording.saved name_recording_stop");
 
                 $datalPAyload = selectCallID($payload['call_leg_id']);
