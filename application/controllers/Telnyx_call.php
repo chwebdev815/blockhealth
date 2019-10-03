726e47f8ada2b2c11db0cca9a0de4535761f397a
Adi <Adi@Adi-PC>
2019-10-03 12:23:14 +0530

    Telnyx setup - log

--- application/controllers/Telnyx_call.php
+++ application/controllers/Telnyx_call.php
@@ -117,7 +117,8 @@ class Telnyx_call extends CI_Controller {
                 base64_decode($payload['client_state']) == "user_response_get") {
 
             log_message("error", "start - speak_ended user_response_get");
-            log_message("error", "payload = " . json_encode($payload));
+            log_message("error", "select data = " . json_encode($selectData));
+            
 
             if ($selectData['step_one'] == 1) {
                 log_message("error", "step1 = 1");
