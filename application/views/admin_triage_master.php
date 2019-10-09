58a92c67ddcacac6182061dd404737500bac7dc3
Adi <Adi@Adi-PC>
2019-10-09 19:57:46 +0530

    3) Status (If caller is 'newpatient', status is '[green] referral triage' or '[red] Awaiting reply' // if caller is 'patient' = then status = '[green] Forwarded' (during business hours) or '[red] Awaiting reply' (not during business hours)

--- application/views/admin_triage_master.php
+++
@@ -1,1 +0,0 @@
-<div class="db-content-inside">    <table class="table table-hover db-table" id="table_admin_triage">        <thead>            <tr>                <th>Patient Name</th>                <th>Referring Physician</th>                <th>Reason for Referral</th>                <th>Status</th>            </tr>        </thead>    </table></div>
